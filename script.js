38874d3517c54dd6795c1b993f80d5dfc6e27803
Chandrima Chakraborty <45023825+chandrima09@users.noreply.github.com>
2026-02-13 14:17:46 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const text = ["Cybersecurity Analyst", "SOC Analyst", "Cloud Security Enthusiast"];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+function type() {
+
+if (count === text.length) {
+count = 0;
+}
+
+currentText = text[count];
+letter = currentText.slice(0, ++index);
+
+document.querySelector(".typing").textContent = letter;
+
+if (letter.length === currentText.length) {
+count++;
+index = 0;
+}
+
+setTimeout(type, 100);
+}
+
+type();
